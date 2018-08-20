0f2d45e69ab28114d7c5f7e7757b5986b376d499
czh_home <5741034@qq.com>
2018-08-21 01:39:38 +0800

    add anko

--- app/src/main/java/czh/fast/sample/mvp/presenter/TestPresenter.kt
+++
@@ -1,9 +0,0 @@
-package czh.fast.sample.mvp.presenter
-
-import czh.fast.sample.mvp.contract.TestContract
-
-class TestPresenter(private val mView: TestContract.View) : TestContract.Presenter {
-    init {
-        mView.presenter = this
-    }
-}
