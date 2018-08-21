6617f76860f30b4eb214a2353ffc4ac76f6cb308
chenzhihui <5741034@qq.com>
2018-08-21 13:43:44 +0800

    add anko

--- app/src/main/java/czh/fast/sample/mvp/ui/adapter/UserAdapter.kt
+++ app/src/main/java/czh/fast/sample/mvp/ui/adapter/UserAdapter.kt
@@ -1,24 +1,16 @@
 package czh.fast.sample.mvp.ui.adapter
 
-import android.view.View
 import czh.fast.sample.db.User
 import czh.fast.sample.mvp.ui.layout.item.DbItemUI
+import czh.library.AnkoAdapter
 import czh.library.BaseViewHolder
-import czh.library.SimpleAnkoAdapter
-import org.jetbrains.anko.AnkoContext
 
-class UserAdapter(data: List<User>?) : SimpleAnkoAdapter<User>(data) {
-    val ui = DbItemUI()
-    override fun ankoLayout(): View {
-        return ui.createView(AnkoContext.create(mContext))
-    }
+class UserAdapter(ui: DbItemUI, data: List<User>?) : AnkoAdapter<DbItemUI, User>(ui, data) {
 
-    override fun convert(holder: BaseViewHolder, item: User?) = with(ui) {
+    override fun convert(holder: BaseViewHolder, ui: DbItemUI, item: User?) = with(ui) {
         tvName.text = item?.urerName
         tvAge.text = item?.urerAge.toString()
-
     }
-
 }
 
 
